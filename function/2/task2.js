e78a607785c191d7f09d439c598361c5a33a723f
Maxim <shidloba@gmail.com>
2023-01-02 22:33:28 +0300

    added_new_task

--- function/2/task2.js
+++ function/2/task2.js
@@ -1,19 +1,34 @@
-// На входе 7 чисел. Все эти значения пользователь вводит с клавиатуры и заполяет 
-// 5 первыми значениями массив. Далее необходимо создать массив из тех 
-// значений, которые являются числами и НЕ равны 2 другим введенным. Если 
-// длина массива равна 0, то вывести ‘Массив пуст’. Добавить проверку на ввод 
+// На входе 7 чисел. Все эти значения пользователь вводит с клавиатуры и заполяет
+// 5 первыми значениями массив. Далее необходимо создать массив из тех
+// значений, которые являются числами и НЕ равны 2 другим введенным. Если
+// длина массива равна 0, то вывести ‘Массив пуст’. Добавить проверку на ввод
 // только чисел после каждого введенного элемента. forEach
 
 const value = 5;
-const arr = [];
-let res = [];
-let x = prompt(`enter value`);
-let y = prompt(`enter value`);
+let arr = [];
+let res[];
+let x = prompt('Enter value');
+let y = prompt('Enter value');
+
 for (let i = 0; i < value; i++) {
-    arr.push(prompt(`enter value`));
+
+    arr.push(prompt('Enter value'));
 }
 arr.forEach(function (elem) {
-    if (!isNaN(elem)!===x||!===y){
-
+    if (!isNaN(elem) && elem !== y) {
+        res.push(elem);
     }
 })
+
+let filtered = arr.filter(function (elem) {
+    if (!isNaN(elem) && elem !== x && elem !== y) {
+        return true;
+    }
+})
+console.log(res);
+
+if (res.length === 0) {
+    console.log('empty');
+} else {
+    console.log('res');
+}
