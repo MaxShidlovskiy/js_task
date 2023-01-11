3de68a77d3602430bc675145f36a8a0ca235d25b
Maxim <shidloba@gmail.com>
2023-01-11 15:01:27 +0300

    create_task_2

--- 22_functiom/6/task6.js
+++ 22_functiom/6/task6.js
@@ -1,17 +1,27 @@
-// На входе статичный объект. Необходимо числовые значения удвоить на выходе.
+// На входе массив. Реализуйте 2 функции. Первая для проверки, что в массиве 
+// только числа. Вторая для получения суммы всех элементов массива. Если 
+// результат функции проверки – true, то вызывать новую функцию, возвращающую 
+// сумму всех элементов массива
 
 
-let obj = {
-    1: `one`,
-    2: 100,
-    3: null,
-    4: 23,
-    5: 17,
-}
-const temp = [];
-for (let key in obj) {
-    if (!isNaN(obj[key]) && typeof obj[key] === `number`) {
-        temp.push(obj[key] * 2)
+const str = prompt(`кол-во элю массива`);
+const doArray = (count) => {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+        arr.push(prompt(`новое число`));
+
     }
+    return arr;
 }
-console.log(temp);
+const array = doArray(str);
+co
+const doSum = (numbers) => {
+    const filtered = numbers.filter(elem => !isNaN(elem));
+    return true;
+}
+
+let result = array.reduce(function (sum, elem) {
+    return sum + elem;
+}, 0);
+
+console.log(result);
