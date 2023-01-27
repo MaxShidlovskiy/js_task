9f2255557e980c9992f9282792e39d388c19e7c4
Maxim <shidloba@gmail.com>
2023-01-27 13:45:12 +0300

    add_new_task

--- string_2/21/task18.js
+++ string_2/21/task18.js
@@ -35,4 +35,20 @@ if (dis < 0) {
     }else{
         console.log(x2,x1)
     }
+}const a = +prompt('Введите первое число');
+const b = +prompt('Введите второе число');
+const c = +prompt('Введите третье число');
+const dis = (b ** 2) - (4 * a * b);
+const x1 = (-b - (Dis ** 0.5)) / (2 * a)
+const x2 = (-b + (Dis ** 0.5)) / (2 * a)
+if (dis < 0) {
+    console.log("корней нет");
+} else if (b === 0) {
+    console.log(-b / (2 * a));
+} else {
+    if (x1 > x2){
+        console.log(x1,x2)
+    }else{
+        console.log(x2,x1)
+    }
 }
