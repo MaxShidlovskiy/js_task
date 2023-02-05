8bf5fcd1284994a32cc74cbf01b4fd57c6a97ab5
Maxim <shidloba@gmail.com>
2023-02-05 22:28:56 +0300

    add_task_event

--- 30_Event/7/app.js
+++ 30_Event/7/app.js
@@ -11,3 +11,17 @@ but.addEventListener(`click`, function () {
         alert(error.message)
     }
 })
+// 
+const but = document.querySelector(`button`);
+but.addEventListener(`click`, function () {
+
+    try {
+        const inp = document.querySelector(`input`);
+        if (inp.value.trim() === ``) throw new Error (`empty string`)
+        if(isNaN(inp.value)) throw new Error(`its string`)
+        const result = document.querySelector(`.result`)
+        result.innerHTML = inp.value * 2
+    } catch (error) {
+        alert(error.message)
+    }
+})
