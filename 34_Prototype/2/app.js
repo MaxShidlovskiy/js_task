fd18e9b83f2f93927ba64010ae99b5ce81f784ca
Maxim <shidloba@gmail.com>
2023-02-14 11:59:04 +0300

    create empty file

--- 34_Prototype/2/app.js
+++ 34_Prototype/2/app.js
@@ -1,14 +1,32 @@
-// Написать функцию на подсчет количества пар ключ значение в объекте. Добавить
-// проверки 
-const obj = {
-    id: 1,
-    name_: ` srten`,
-    date: `month`,
-}
+// По условию задачи даны инпут и кнопка. Пользователь вводит в инпут значения и
+// по нажатию на кнопку формирует массив строк. Необходимо вывести: полный
+// массив из всех элементов, а также массив из уникальных значений
 
-function doCount(obj_) {
-    const arr = Object.entries(obj_)
-    return arr.length
-}
-let result = doCount(obj)
-console.log(result);
+const input = document.querySelector(`input`);
+const array = document.querySelector(`.array`);
+const unique = document.querySelector(`.unique`);
+const button = document.querySelector(`button`);
+let arr = [];
+
+button.addEventListener(`click`, function () {
+    try {
+        if (input.value == "") {
+            throw new Error(`empty value`);
+        }
+        arr.push(input.value);
+        array.innerHTML = arr;
+
+        const uniqueArr = [];
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+            if (!uniqueArr.includes(element)) {
+                uniqueArr.push(element);
+            }
+
+        }
+        unique.innerHTML = uniqueArr;
+        input.value = ``;
+    } catch (error) {
+        alert(error.message);
+    }
+});
