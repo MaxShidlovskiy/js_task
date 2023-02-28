b6c11b1b0dc61638037cadfa862f3f7c541f21fc
Maxim <shidloba@gmail.com>
2023-02-28 12:38:40 +0300

    add task 37

--- 37/4/app.js
+++ 37/4/app.js
@@ -1,55 +1,54 @@
-// Реализуйте класс ServerById. Обязательными функциями считаются функции 
-// middleware, controller, service, repository. Цепочка взаимодействия между 
-// методами следующая:
-// middleware -> controller -> service -> repository, где:
-// middleware – функция валидатор 
-// controller – функция, принимающая данные. Принимает json
-// service – функция проверки на то что с repository вернулось значение
-// repository – функция, симулирующая БД. Хранит массив данных. Взаимодействие с 
-// этим массивом осуществляется только в repository. Массив находится в 
-// приложении
-// Задание:
-// на вход подается JSON вида: 
-// `{ 
-// "id": 1
-// }`
-// Необходимо вывести в консоль найденный элемент массива по id если таковой 
-// имеется. В противном случае бросить исключение. Добавить проверки
+// Реализовать следующие классы Круг, Треугольник и Квадрат, которые наследуются от Figure. 
+// Каждый экземпляр класса должен содержать свойства Площадь и Имя. 
+// Вывести всю информацию о фигурах в консоль. Площадь должна рассчитываться по математическим формулам.
 
-class ServerById {
-  middleware(object) {
-    if (!object.hasOwnProperty(`id`)) throw new Error(`Поле id не передано`)
-    if (isNaN(object.id)) throw new Error(`this is string`);
-  }
-  controller(object) {
-    try {
-      this.middleware()
-      const data = this.service(object);
-      return data;
-    } catch (error) {
-      alert(error.message)
-    }
-  }
-  service(object) {
-    const data = this.repository(object);
-    return data;
-  }
+// Класс Figure содержит поля: 
+// p
+// r
+// a
+// h
 
-  repository(object) {
-    const array = [{ "id": 1, "name": "Yesenia", "age": 22 },
-    { "id": 2, "name": "Hanna", "age": 22 },
-    { "id": 3, "name": "Stanislau", "age": 25 },
-    { "id": 4, "name": "German", "age": 18 },
-    { "id": 5, "name": "Maria", "age": 27 }
-    ]
-    const filtered = array.filter((elem) => elem.id == object)
-    return filtered;
+// Дочерние классы содержаь геттеры на получение площади
+
+class Figure {
+  r;
+  p = 3.14;
+  s;
+  h;
+  name;
+
+  setR(radius) {
+    this.r = radius;
+  }
+  setS(side) {
+    this.s = side;
+  }
+  setH(height) {
+    this.h = height;
+  }
+  setName(name) {
+    this.name = name;
   }
 }
-const object = JSON.parse(`{
-  "id": 1
-}`)
+class Circle extends Figure {
+  getSquare() {
+    return this.p = this.r ** 2 
+  }
+}
+class Triangle extends Figure {
+  getSquare() {
+    return 1 / 2 *this.s * this.h;
+  }
+}
+let circle = new Circle();
+circle.setR(3);
+console.log(circle.getSquare());
 
-let serverById = new ServerById();
-const result = serverById.controller(object);
-console.log(result);
+console.log(`------------------------------------------------`);
+
+let triangle = new Triangle();
+triangle.setA(4);
+triangle.setH(7);
+console.log(triangle.getSquare());
+
+console.log(`------------------------------------------------`);
