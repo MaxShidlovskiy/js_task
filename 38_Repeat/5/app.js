6ff13333ca0ff655af1055bb37efaac2f830b259
Maxim <shidloba@gmail.com>
2023-03-06 10:42:49 +0300

    create empty files

---
+++ 38_Repeat/5/app.js
@@ -0,0 +1,22 @@
+// На входе n – количество элементов массива. Далее производится заполнение
+// массива с клавиатуры. Необходимо создать функцию возвращающую новый
+// массив из отфильтрованных значений, где строка начинается на [a, h].
+// [“ann”, “school”, “hschool”, “borabora”] -> [“ann”, “hschool”]
+
+const n = promp(`enter the value`);
+try {
+  isvalid(arr)
+} catch (error) {
+
+}
+
+filteredArray(arr){
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+const array = doArray(n);
+const newArr = filtere
