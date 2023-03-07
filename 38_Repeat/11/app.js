dedd232490122733b980164b98a3b3a9d556ae70
Maxim <shidloba@gmail.com>
2023-03-08 00:21:32 +0300

    create empty files

--- 38_Repeat/11/app.js
+++ 38_Repeat/11/app.js
@@ -1,16 +1,25 @@
-// Вывести в строчку все вводимые значения через инпут в виде массива. Добавить
-// проверку, что поле не пустое
+// Вывести в строчку все вводимые значения через инпут в виде массива, а также
+// все удвоенные значения данного массива в div ниже. Добавить проверку, что
+// поле не пустое
+
 
 const inp = document.querySelector(`input`);
 const btn = document.querySelector(`button`);
-const result = document.querySelector(`div`)
+const result = document.querySelector(`.res`)
+const dbl = document.querySelector(`.double`)
 const arr = [];
 
 btn.addEventListener(`click`, function () {
   try {
-    if (!inp.value ) throw new Error(``);
-    if (isNaN(inp.value)) throw new Error(`это не число`);
-    result.innerHTML = inp.value * 2;
+    let elem = inp.value;
+    if (!elem) throw new Error(`пустое значение`);
+    if (isNaN(elem)) throw new Error(`это не число`);
+
+    arr.push(elem);
+    result.innerHTML = arr;
+    dbl.innerHTML = arr.map(function (el) {
+      return el * 2;
+    })
   } catch (error) {
     alert(error.message)
   }
