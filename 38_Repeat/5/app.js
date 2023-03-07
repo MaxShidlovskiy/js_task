f36885fa489ce23c00d5ba381d1f5f560be0a803
Maxim <shidloba@gmail.com>
2023-03-07 23:13:46 +0300

    create empty files

--- 38_Repeat/5/app.js
+++ 38_Repeat/5/app.js
@@ -3,20 +3,39 @@
 // массив из отфильтрованных значений, где строка начинается на [a, h].
 // [“ann”, “school”, “hschool”, “borabora”] -> [“ann”, “hschool”]
 
-const n = promp(`enter the value`);
-try {
-  isvalid(arr)
-} catch (error) {
+const n = prompt(`enter the length`);
 
+const doArray = (length) => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(prompt(`enter the value`));
+  }
+  return arr;
 }
 
-filteredArray(arr){
+const filteredArray = (arr) => {
   try {
-    
+    isValid(arr)
+    const newArr = arr.filter(elem => {
+      if (elem[0] === `a` || elem[0] === `h`) {
+        return true;
+      }
+    })
+    return newArr
   } catch (error) {
-    
+    return error.message;
   }
 }
+const isValid = (arr) => {
+  arr.forEach(element => {
+    if (!isNaN(element)) {
+      throw new Error(`not a string`)
+    }
+  });
+}
+
+
 
 const array = doArray(n);
-const newArr = filtere
+const newArr = filteredArray(array)
+console.log(newArr);
