79a2c9b27b7f771758402e5cbee9bd98051e5f99
Maxim <shidloba@gmail.com>
2023-03-24 22:55:13 +0300

    create empty files

--- ~11_number/8/task8.js
+++ ~11_number/8/task8.js
@@ -1,6 +1,7 @@
 // Найдите квадратный корень из 245. Предварительно изучите модуль Math и в
 // отдельности Math.sqrt
+const sqrt = Math.sqrt(245);
 
-console.log( Math.sqrt(245));
+console.log(sqrt);
 
   
