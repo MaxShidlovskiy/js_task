ee0006ebb91af979b180adf512f3928776eac71f
Maxim <shidloba@gmail.com>
2023-03-29 13:42:54 +0300

    create empty files

--- 46_repeat/4/app.js
+++ 46_repeat/4/app.js
@@ -1,54 +1,11 @@
-// Реализовать следующие классы Круг, Треугольник и Квадрат, которые наследуются от Figure. 
-// Каждый экземпляр класса должен содержать свойства Площадь и Имя. 
-// Вывести всю информацию о фигурах в консоль. Площадь должна рассчитываться по математическим формулам.
-
-// Класс Figure содержит поля: 
-// p
-// r
-// a
-// h
-
-// Дочерние классы содержаь геттеры на получение площади
-
-class Figure {
-  r;
-  p = 3.14;
-  s;
-  h;
-  name;
-
-  setR(radius) {
-    this.r = radius;
-  }
-  setS(side) {
-    this.s = side;
-  }
-  setH(height) {
-    this.h = height;
-  }
-  setName(name) {
-    this.name = name;
-  }
-}
-class Circle extends Figure {
-  getSquare() {
-    return this.p = this.r ** 2 
-  }
-}
-class Triangle extends Figure {
-  getSquare() {
-    return 1 / 2 *this.s * this.h;
-  }
-}
-let circle = new Circle();
-circle.setR(3);
-console.log(circle.getSquare());
-
-console.log(`------------------------------------------------`);
-
-let triangle = new Triangle();
-triangle.setA(4);
-triangle.setH(7);
-console.log(triangle.getSquare());
-
-console.log(`------------------------------------------------`);
+// Реализуйте класс ServerPost. Обязательными функциями считаются функции middleware, 
+// controller, service, repository. Цепочка взаимодействия между методами 
+// следующая:middleware -> controller -> service -> repository, где:middleware – функция валидатор 
+// controller – функция, принимающая данные. Принимает jsonservice – функция проверки на то что с 
+// repository вернулось значениеrepository – функция, симулирующая БД. Хранит массив данных. 
+// Взаимодействие с этим массивом осуществляется только в repository. 
+// Массив находится в приложенииЗадание:на вход подается JSON вида: ` { "email": “test@mail.ru", "pwd": “test" }`
+// Необходимо добавить в массив БД объект только в том случае, если нет совпадений по name. Если совпадения нет, 
+// то в объект клиента добавить ключ id с последним возможным уникальным id БД, таким образом, 
+// чтобы в БД был запушен объект вида{"id": 6, "email": "yesenia@mail.ru", "pwd": "pwdffff" }
+// Если совпадение есть – ошибка. Добавить проверки 
