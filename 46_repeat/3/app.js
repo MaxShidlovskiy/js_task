404b95ff3e882382d4db92e1aa10a5a67f7179d7
Maxim <shidloba@gmail.com>
2023-03-31 23:54:42 +0300

    create task 46

--- 46_repeat/3/app.js
+++ 46_repeat/3/app.js
@@ -4,51 +4,11 @@
 //  Класс Singer состоит из свойств years, birth, style и метода getMoreInfo(), 
 //  который выводит информацию о певце: name, surname years, birth, style. 
 //  Необходимо вызвать метод getMoreInfo
-class Worker {
-    name;
-    surname;
-    getFullName() {
-        return `${this.name}${this.surname}`;
+class HTML{
+    bindOptionPlus(){
+        let displayTag = document.querySelector('.display');
+        let btnPlusTag = document.querySelector(".plus");
+    btnPlusTag = add.EventListener('click', ()=>{
+        displayTag.innerHTML = +displayTag.textContent+1;
+    })
     }
-    setName(name) {
-        this.name = name;
-    }
-    setSurname(surname) {
-        this.surname = surname;
-    }
-
-}
-class Singer extends Worker {
-    years;
-    birthday;
-    style;
-    getMoreInfo() {
-        return `${this.name} ${this.surname} ${this.years} ${this.birthday} ${this.style}`;
-    }
-    setYears(years) {
-        this.years = years;
-    }
-    setBirthday(birthday) {
-        this.birthday = birthday;
-    }
-    setStyle(style) {
-        this.style = style;
-    }
-}
-let worker = new Worker();
-
-worker.setName(`Jhon`);
-worker.setSurname(`Smith`);
-let result = worker.getFullName();
-console.log(result);
-
-console.log(`---------------------------------------------`);
-
-let singer = new Singer();
-singer.setName(`Karl`);
-singer.setSurname(`Smith`);
-singer.setYears(30);
-singer.setBirthday(`01.01.1999`);
-singer.setStyle(`pop`);
-let result2 = singer.getMoreInfo();
-console.log(result2);
