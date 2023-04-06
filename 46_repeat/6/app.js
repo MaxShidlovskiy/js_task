a6d289ddd1dee217865a046bf7ddb5e58b5f028c
Maxim <shidloba@gmail.com>
2023-04-07 00:14:09 +0300

    create empty files

--- 46_repeat/6/app.js
+++ 46_repeat/6/app.js
@@ -1,39 +1,14 @@
-// На входе n – количество элементов массива. Далее производится заполнение
-// массива с клавиатуры. Необходимо создать функцию возвращающую новый
-// массив из элементов, каждое значение которого имеет вид #name.
-// [“hschool”, “company”] -> [“#hschool”, “#company”]
+// 6. Допишите функционал регистрации. Небезопасно хранить в БД пароль в
+// пользовательском виде. Необходимо написать алгоритм шифрования с помощью
+// шифра Цезаря.
+// Задание: На сервер подается объект с ключом pwd, как и сделано в предыдущем
+// задании. Пароль – строка текста на английском языке, в которой нужно
+// зашифровать все слова. Каждое слово строки следует зашифровать с помощью
+// шифра Цезаря (циклического сдвига на длину этого слова). Буквы верхнего и
+// нижнего регистров при этом не изменяют свой первоначальный регистр
 
-function createArr() {
-  const n = prompt(`length of arr`);
-  let arr = [];
-  for (let i = 0; i < n; i++) {
-    const element = +prompt(`put the element`);
-    arr.push(element)
-  }
-  return arr;
-}
-
-function doHashTag(array) {
-  try {
-    isValid(array);
-    let newArr = [];
-    for (let i = 0; i < array.length; i++) {
-      const element = `#` + array[i];
-      newArr.push(element)
-    }
-    return newArr
-  } catch (error) {
-    return(error.message)
-  }
-}
-
-
-function isValid(array) {
-  array.forEach(element => {
-    if (!isNaN(element)) throw new Error(`Это число`)
-  });
-}
-
-let temp = createArr()
-let t = doHashTag(temp);
-console.log(t);
+class Client {
+  doRegistration() {
+      const btn = document.querySelector('button');
+      const email = document.querySelector('.email');
+      const password = document.querySelector('.password');
