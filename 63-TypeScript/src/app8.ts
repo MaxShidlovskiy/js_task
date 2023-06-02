a4f1023a02e9ee404bd0f4cbd460e346bad24035
Maxim <shidloba@gmail.com>
2023-06-02 11:51:17 +0300

    create task 8

---
+++ 63-TypeScript/src/app8.ts
@@ -0,0 +1,8 @@
+// Создайте переменную str и присвойте ей строковое значение. Используя цикл for, 
+// выведите каждый второй символ строки
+
+let str8: string = `superstring`;
+
+for (let i: number = 0; i < str8.length; i++) {
+   console.log(str8[i]);
+}
