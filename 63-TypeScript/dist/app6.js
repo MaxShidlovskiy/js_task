083f94f0778e2aa414962e051a8a8100ceec73b8
Maxim <shidloba@gmail.com>
2023-06-03 12:06:34 +0300

    create task 6

--- 63-TypeScript/dist/app6.js
+++ 63-TypeScript/dist/app6.js
@@ -1,15 +1,11 @@
 // Напишите программу, которая проверяет, является ли заданная строка 
 // палиндромом.
-
-let str6: string = prompt(`Введите слово для проверки`);
-
+let str6 = prompt(`Введите слово для проверки`);
 if (str6 === str6.split(``).reverse().join(``)) {
     console.log(`это палиндром`);
-} else {
+}
+else {
     console.log(`это не палиндром`);
 }
-
 (str6 === str6.split(``).reverse().join(``)) ? console.log(`это палиндром`) : console.log(`это не палиндром`);
-
 alert(str6 === str6.split(``).reverse().join(``) ? `это палиндром` : `это не палиндром`);
-
