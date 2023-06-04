6cceff24dc7634ebf6a7b12e04f6de70d6fb7326
Maxim <shidloba@gmail.com>
2023-06-04 12:24:43 +0300

    create task 9

---
+++ 63-TypeScript/src/app9.ts
@@ -0,0 +1,13 @@
+// Напишите программу, которая принимает строку и возвращает новую строку, в 
+// которой все символы уникальны (не повторяются). Например, для строки "hello" 
+// результатом должна быть строка "helo".
+
+let str9: string = `hello`;
+let result9: string = ``;
+
+for (let i =0: number; i < str9.length; i++) {
+    if (!result9.includes(str9[i])) {
+        result9 += str9[i];
+    }
+}
+console.log(result9);
