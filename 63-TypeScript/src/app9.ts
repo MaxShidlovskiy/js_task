140bd7b3f5c6de4ecad29dd201b24357ff4ac03f
Maxim <shidloba@gmail.com>
2023-06-05 10:20:08 +0300

    create task 9

--- 63-TypeScript/src/app9.ts
+++ 63-TypeScript/src/app9.ts
@@ -5,7 +5,7 @@
 let str9: string = `hello`;
 let result9: string = ``;
 
-for (let i =0: number; i < str9.length; i++) {
+for (let i: number = 0; i < str9.length; i++) {
     if (!result9.includes(str9[i])) {
         result9 += str9[i];
     }
