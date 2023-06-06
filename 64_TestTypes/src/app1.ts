c16aea464c70e8ac3b6394912b12236a6244ec4a
Maxim <shidloba@gmail.com>
2023-06-06 11:56:44 +0300

    create task 1

---
+++ 64_TestTypes/src/app1.ts
@@ -0,0 +1,11 @@
+//  Создайте массив чисел и найдите сумму всех нечетных элементов в массиве. \
+
+const array1: number[] = [1, 2, 3, 4, 5]
+
+const filtered = array1.filter(function (el) {
+    return el % 2 === 1 ? true : false
+})
+const result = filtered.reduce(function (sum, elem) {
+    return sum + elem
+}, 0)
+console.log(result);
