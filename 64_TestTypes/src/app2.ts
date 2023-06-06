ccd8e39a46e1bbeb86b44eb987c64591e6215437
Maxim <shidloba@gmail.com>
2023-06-06 12:06:08 +0300

    create task 2

--- 64_TestTypes/src/app2.ts
+++ 64_TestTypes/src/app2.ts
@@ -1,2 +1,10 @@
 // Создайте массив строк и выведите на экран все элементы, которые содержат 
 // букву 'a'.
+
+let str2: string[] = [`la hehe no`, `ok yea`, `oops queuq`];
+
+for (let i: number = 0; i < str2.length; i++) {
+    if (str2[i].includes(`a`)){
+        console.log(str2[i]);        
+    }
+}
