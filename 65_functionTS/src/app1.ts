54731bc0e8a64d7ce31d6236fc34b24fc0e87efa
Maxim <shidloba@gmail.com>
2023-06-09 11:45:33 +0300

    create task 1

--- 65_functionTS/src/app1.ts
+++ 65_functionTS/src/app1.ts
@@ -6,23 +6,19 @@ interface iThings {
     count: number;
     price: number
 }
-let arr1 = [
+let arr1: iThings[] = [
     { id: 1, title: 'Часы', count: 10, price: 500 },
     { id: 2, title: 'Смартфон', count: 33, price: 1500 },
     { id: 3, title: 'Моноблок', count: 6, price: 2200 },
     { id: 4, title: 'Ноутбук', count: 13, price: 3000 },
     { id: 5, title: 'Планшет', count: 22, price: 2100 },
 ]
-function getStorageByCount(arr1) {
-    let filtered1 = arr1.filter((elem) => {
-        if (elem.price > 10) {
-            return elem;
-        }
-    })
+function getStorageByCount(arr1: iThings[]): iThings[] {
+    let filtered1: iThings[] = arr1.filter((elem: iThings) => elem.price > 10)
     return filtered1;
 }
 
-let result1 = getStorageByCount(arr1);
+    let result1:iThings[] = getStorageByCount(arr1);
 
-console.log(result1);
+    console.log(result1);
 
