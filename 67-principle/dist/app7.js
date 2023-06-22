e244d3eef1a04e20f72e8fb38e2a5240134eb93e
Maxim <shidloba@gmail.com>
2023-06-22 21:58:02 +0300

    complete task 7

--- 67-principle/dist/app7.js
+++ 67-principle/dist/app7.js
@@ -1,23 +1,6 @@
-// Создайте класс TodoList, который содержит свойство tasks (список задач) и методы
-// addTask() (добавление задачи), removeTask() (удаление задачи) и getTasks()
-// (получение списка задач). Создайте экземпляр класса TodoList и выполните
-// несколько операций.
-class TodoList7 {
-    tasks = [`работа`, `поесть`, `покодить`, `поспать`];
-    addTask(title) {
-        this.tasks.push(title);
-        return this.tasks.join(` `);
-    }
-    removeTask() {
-        this.tasks.pop();
-        return this.tasks.join(` `);
-    }
-    getTasks() {
-        return this.tasks.join(` `);
-    }
+// Создайте класс StringManipulator, который будет иметь методы reverseString,
+// isPalindrome, countVowels. Реализуйте функционал для разворота строки,
+// проверки, является ли строка палиндромом, и подсчета гласных букв в строке.
+// Использовать Generics
+class StringManipulator {
 }
-const todoList7 = new TodoList7();
-console.log(todoList7.getTasks());
-console.log(todoList7.removeTask());
-console.log(todoList7.addTask(`wolking`));
-console.log(todoList7.getTasks());
