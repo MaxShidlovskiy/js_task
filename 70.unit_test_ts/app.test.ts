1dd6e532f9aa9db0924a3cee2b542149db8f52f7
Maxim <shidloba@gmail.com>
2023-07-02 00:37:45 +0300

    create test 8

--- 70.unit_test_ts/app.test.ts
+++ 70.unit_test_ts/app.test.ts
@@ -1,4 +1,4 @@
-import { isPalindrome, calculateFactorial, capitalizeString, flattenArray } from "./app";
+import { StringArray, isPalindrome, calculateFactorial, capitalizeString, flattenArray } from "./app";
 
 describe(`тест 1`, () => {
     test(`Success`, () => {
@@ -42,21 +42,36 @@ describe(`тест 3`, () => {
 
 })
 
-describe(`тест 4`, () => {
-    test(`Success`, () => {
-        const result = flattenArray([1, 2, [3]])
-        expect(result).toEqual([1, 2, 3])
+describe('тест 4', () => {
+    test('Success', () => {
+        const result = flattenArray([1, 2, [3]]);
+        expect(result).toEqual([1, 2, 3]);
     })
-    test(`Success`, () => {
-        const result = flattenArray([1, 2, [3, 4, 5, [6, [7, 8]]]])
-        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+    test('Success', () => {
+        const result = flattenArray([1, 2, [3, 4, 5, [6, [7, 8]]],]);
+        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
     })
-    test(`Success`, () => {
+    test('Success', () => {
         const result = flattenArray("string");
-        expect(result).toBe(`not array`);
+        expect(result).toBe('not array');
     })
-    test(`Success`, () => {
-        const result = flattenArray([])
-        expect(result).toBe(`empty array`)
+    test('Success', () => {
+        const result = flattenArray([]);
+        expect(result).toBe('empty array');
     })
+    test('Success', () => {
+        const result = flattenArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    })
+
+})
+
+describe('тест 5', () => {
+    test('success', () => {
+        const stringArray = new StringArray()
+        const result1 = stringArray.getLongestWord([`eee`, `ggg`, `lllllllllllll`, `ggg` ]) 
+        const result2 = stringArray.getUniqueWords([`eee`, `ggg`, `lllllllllllll`, `ggg`  ]) 
+        expect(result1).toBe(`lllllllllllll`)
+        expect(result2).toEqual([`eee`, `ggg`, `lllllllllllll`])
+    })  
 })
