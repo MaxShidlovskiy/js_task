3c7a0efec96cf786235ad8ff5313718e6841d0bf
Maxim <shidloba@gmail.com>
2023-07-07 21:41:43 +0300

    create task 13

--- 64_TestTypes/dist/app13.js
+++ 64_TestTypes/dist/app13.js
@@ -2,12 +2,21 @@
 // четности и нечетности, в соответствии со значением числа:
 // [1, 2, 3, 4, 5, 6] -> [“нечет”, “чет”, “нечет”, “чет”, “нечет”, “чет”]
 let arr13 = [1, 2, 3, 4, 5, 6];
-let result13 = arr13.map((elem) => {
-    if (elem % 2 == 0) {
-        return `чет`;
-    }
+// let result13: string[] = arr13.map((elem: number) => {
+//     if (elem % 2 == 0) {
+//         return `чет`
+//     } else {
+//         return `нечет`
+//     }
+// })
+// console.log(result13);
+//for
+let result13 = [];
+for (let i = 0; i < arr13.length; i++) {
+    if (arr13[i] % 2 == 0)
+        result13.push(`chet`);
     else {
-        return `нечет`;
+        result13.push(` nechet`);
     }
-});
+}
 console.log(result13);
--- 64_TestTypes/src/app13.ts
+++ 64_TestTypes/src/app13.ts
@@ -4,12 +4,23 @@
 
 let arr13: number[] = [1, 2, 3, 4, 5, 6];
 
-let result13: string[] = arr13.map((elem: number) => {
-    if (elem % 2 == 0) {
-        return `чет`
-    } else {
-        return `нечет`
-    }
-})
+// let result13: string[] = arr13.map((elem: number) => {
+//     if (elem % 2 == 0) {
+//         return `чет`
+//     } else {
+//         return `нечет`
+//     }
+// })
+
+// console.log(result13);
+
+//for
+
+let result13: string[] = [];
+
+for (let i: number = 0; i < arr13.length; i++) {
+    if (arr13[i] % 2 == 0) result13.push(`chet`)
+    else { result13.push(` nechet`) }
+}
 
 console.log(result13);
