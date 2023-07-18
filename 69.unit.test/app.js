30de22dcc999630b99017657841d86dcf4a337fe
Maxim <shidloba@gmail.com>
2023-07-18 22:05:23 +0300

    create empty files

---
+++ 69.unit.test/app.js
@@ -0,0 +1,61 @@
+// 1. Написать функцию которая принимает 2 числа и возводит 1 во 2 степень. 
+// Добавить необходимые проверки.
+// Написать тест для функции
+
+function sum(a, b) {
+    try {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error(`не число`)
+        }
+        return a ** b;
+    } catch (error) {
+        return error.message
+    }
+}
+
+// 2. Написать функцию которая принимает 2 числа и возвращает результат 
+// произведения 2 множителей. Добавить необходимые проверки.
+// Написать тест для функции
+
+function multiply(a, b) {
+    try {
+        if (typeof a !== `number` || typeof b !== `number`) {
+            throw new Error(`не число`)
+        }
+        return a * b
+    } catch (error) {
+        return error.message
+    }
+}
+
+// 3. Написать функцию которая принимает массив чисел и находит сумму всех 
+// элементов. Добавить необходимые проверки.
+// Написать тест для функции
+
+function doArray(array) {
+    try {
+        if (!Array.isArray(array)) throw new Error(`это не массив`);
+        if (!array.length) throw new Error(`массив пуст`);
+        const bool = array.every(el => typeof el == `number`);
+        if (!bool) throw new Error(`массив содержит строку`);
+        return array.reduce((acc, next) => acc + next, 0)
+    } catch (error) {
+        return error.message
+    }
+}
+
+
+// 4. У вас есть массив объектов вида приведенного в приложении. Необходимо 
+// вывести все товары, количество которых больше 10 и произведены в Германии
+// Написать тест для функции
+
+function findProduct(arr) {
+    try {
+        if (!Array.isArray(arr)) throw new Error(`это не массив`);
+        let filtered = arr.filter(el => el.count > 10 && el.producer == `Германия`)
+        return filtered
+    } catch (error) {
+        return error.message
+    }
+}
+module.exports = { sum, multiply, doArray, findProduct };
---
+++ 69.unit.test/app.test.js
@@ -0,0 +1,66 @@
+const { sum, multiply, doArray, findProduct } = require('./app');
+
+describe(`набор тестов 1`, () => {
+    test(`успешно`, () => {
+        const result = sum(2, 2);
+        expect(result).toBe(4);
+    });
+    test(`успешно`, () => {
+        const result = sum(2, `string`);
+        expect(result).toBe(`не число`);
+    });
+});
+
+describe(`набор тестов 2`, () => {
+    test(`успешно`, () => {
+        const result = multiply(2, 3);
+        expect(result).toBe(6);
+    });
+    test(`успешно`, () => {
+        const result = multiply(2, `string`);
+        expect(result).toBe(`не число`);
+    });
+});
+
+describe(`набор тестов 3`, () => {
+    test(`успешно`, () => {
+        const result = doArray([1, 2, 3]);
+        expect(result).toBe(6);
+    });
+    test(`успешно`, () => {
+        const result = doArray(`string123`);
+        expect(result).toBe(`это не массив`);
+    });
+    test(`успешно`, () => {
+        const result = doArray([]);
+        expect(result).toBe(`массив пуст`);
+    });
+    test(`успешно`, () => {
+        const result = doArray([10, 10, `string`]);
+        expect(result).toBe(`массив содержит строку`);
+    });
+});
+
+describe(`набор тестов 4`, () => {
+    let arr = [
+        { id: 1, title: 'Часы', count: 10, price: 500, producer: 'Германия' },
+        { id: 2, title: 'Смартфон', count: 33, price: 1500, producer: 'Германия' },
+        { id: 3, title: 'Моноблок', count: 6, price: 2200, producer: 'Германия' },
+        { id: 4, title: 'Ноутбук', count: 13, price: 3000, producer: 'Китай' },
+        { id: 5, title: 'Планшет', count: 22, price: 2100, producer: 'Китай' },
+        { id: 6, title: 'Телевизор', count: 4, price: 4100, producer: 'Германия' },
+        { id: 7, title: 'Холодильник', count: 11, price: 2400, producer: 'Германия' }
+    ];
+
+    test(`успешно`, () => {
+        const result = findProduct(arr);
+        expect(result).toEqual([{ id: 2, title: 'Смартфон', count: 33, price: 1500, producer: 'Германия' }, { id: 7, title: 'Холодильник', count: 11, price: 2400, producer: 'Германия' }]);
+        expect(result).toHaveLength(2)
+        expect(result.length).toBeGreaterThan(0)
+        expect(result.length).toBe(2)
+    });
+    test(`успешно`, () => {
+        const result = findProduct(`ttt`);
+        expect(result).toBe(`это не массив`);
+    });
+});
