2eb4137fe9dac209c43a50c3a001dd5c05a1ce6b
Maxim <shidloba@gmail.com>
2023-09-03 09:37:34 +0300

    create app

---
+++ 85_redux/src/App.jsx
@@ -0,0 +1,22 @@
+import Task1 from './page/Task1'
+import Task2 from './page/Task2'
+import Task3 from './page/Task3'
+import Task4 from './page/Task4'
+import Task5 from './page/Task5'
+import Task6 from './page/Task6'
+
+function App() {
+
+  return (
+    <>
+      <Task1 />
+      <Task2 />
+      <Task3 />
+      <Task4 />
+      <Task5 />
+      <Task6 />
+    </>
+  )
+}
+
+export default App
